21a28ad77ab18173512e4d452e3520139fc61216
Ansh Gandhare <anshgag.ai@gmail.com>
2026-09-07 20:31:34 +0530

    Phonepe Expense tracker

---
+++ main.py
@@ -0,0 +1,39 @@
+from src.utils.logger import setup_logger
+from src.services.tracker import ExpenseTrackerService
+from src.config.setting import BUDGET_LIMIT
+
+
+def main():
+    logger = setup_logger()
+    logger.info("Initializing PhonePe Expence Tracker....")
+
+    service = ExpenseTrackerService()
+    summary = service.generate_summary()
+    service.save_summary(summary)
+
+    print("\n" + "="*50)
+    print(" PHONEPE EXPENSE ANALYSIS REPORT ")
+    print("="*50 + "\n")
+    print(f"Total Money Spent: ₹{summary.total_spent:.2f}")
+    print(f"\nSpending Breakdown by Category:")
+
+    for category, amount  in summary.category_totals.items():
+        print(f"     {category:12s} : ₹{amount:.2f}")
+
+    print("-"*50)
+
+    if summary.is_over_budget:
+        print(f"\n WARNING: You have exceeded your monthly budget limit of ₹{BUDGET_LIMIT}")
+    else:
+        print(f"\n Congratulations! You are within your budget limit of ₹{BUDGET_LIMIT}")
+
+    print("="*50 + "\n")
+
+
+if __name__=="__main__":
+    main()
+
+
+
+
+
---
+++ src/utils/file_handler.py
@@ -0,0 +1,25 @@
+"""Read transaction data from a text file and write summary to a JSON file"""
+import json
+import logging
+from pathlib import Path
+
+logger = logging.getLogger("PhonePeTracer")
+
+def read_text_file(file_path: Path) -> list[str]:
+    """ Read a text file and return a list of lines"""
+    if file_path.exists():
+        with open(file_path, "r", encoding="utf-8") as f:
+            return f.readlines()
+    else:
+        logger.error(f"File not found: {file_path}")
+        return []
+
+def write_json_file(data: dict, output_path: Path) -> None:
+    """ Write data to a JSON file"""
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+    logger.info(f"Data written to {output_path}")
+
+
+
+    
